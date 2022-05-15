8a0b94f1875bc67e10cfc251b9df0f9a030676b6
Srishti Majumder <srishtimajumder2002@gmail.com>
2022-05-16 04:58:42 +0530

    new changes made

---
+++ to-do-list/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import {Navbar} from './components/navbar';
+import { School } from './components/school';
+function App() {
+  return (
+    <>
+    <div className="App">
+      {/* <Navbar /> */}
+       <School/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ to-do-list/src/components/school.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Stack, Typography,Container } from '@mui/material'
+
+export const School = () => {
+  return (
+    <Container>
+    <Stack direction='row' spacing={3} >
+      <Typography variant='h6'>
+            Monday
+      </Typography>
+      <Typography variant='h6'>
+            Monday
+      </Typography>
+      <Typography variant='h6'>
+            Monday
+      </Typography>
+      <Typography variant='h6'>
+            Monday
+      </Typography>
+      <Typography variant='h6'>
+            Monday
+      </Typography>
+    </Stack>
+    </Container>
+  )
+}
